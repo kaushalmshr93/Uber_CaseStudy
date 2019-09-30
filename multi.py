be842f6a00f3eefc05e7af1e9c9135a2d018fd53
Kaushal Kashyap <GS-1551@GSLAB.COM>
2019-10-01 00:28:44 +0530

    adding new .py file just for test purpose

---
+++ multi.py
@@ -0,0 +1,11 @@
+import numpy as np 
+  
+# input two matrices 
+mat1 = ([1, 6, 5],[3 ,4, 8],[2, 12, 3]) 
+mat2 = ([3, 4],[5, 6],[6,56]) 
+  
+# This will return dot product 
+res = np.dot(mat1,mat2) 
+  
+# print resulted matrix 
+print(res) 
